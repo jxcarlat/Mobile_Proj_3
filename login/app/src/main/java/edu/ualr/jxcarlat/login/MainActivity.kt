51c61302c05a158620e47fb68718ac6d471b4694
Josh Carlat <jxcarlat@ualr.edu>
2018-11-05 21:00:28 -0600

    push to laptop

--- login/app/src/main/java/edu/ualr/jxcarlat/login/MainActivity.kt
+++ login/app/src/main/java/edu/ualr/jxcarlat/login/MainActivity.kt
@@ -1,27 +1,30 @@
 package edu.ualr.jxcarlat.login
 
 import android.content.Intent
+import android.net.Network
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
-import kotlinx.coroutines.experimental.async
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
+import android.widget.EditText
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import okhttp3.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
-import com.google.gson.Gson
-import java.lang.Exception
 
-const val Key_Authorization: String = "authorized_user_ualr"
-
+const val USERNAME: String = "username"
+const val PASSWORD: String = "password"
+const val PASSWORD2: String = "confirm"
 
 
 class MainActivity : AppCompatActivity() {
 
+    val username = findViewById(R.id.editText) as EditText
+    val password = findViewById(R.id.editText2) as EditText
+    val confirmation = findViewById(R.id.editText3) as EditText
     private lateinit var client: OkHttpClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +38,9 @@ class MainActivity : AppCompatActivity() {
         super.onResume()
 
         val myUrl = "http://messenger.mattkennett.com/api-auth/v1/registration/"
+        val formBody: RequestBody = FormBody.Builder()
+                .add("action", "authentication")
+                .build()
         val request: Request = Request.Builder()
                 .url(myUrl)
                 .build()
@@ -59,19 +65,30 @@ class MainActivity : AppCompatActivity() {
 
                 val gson = Gson()
 
-                val myUser: GithubUser = gson.fromJson(responseBody, GithubUser::class.java)
+                val requestBody: String = response.body()!!.string()
+                Log.d("MPK_UTILITY", requestBody)
 
-                updateText = myUser.login + "\n" + myUser.url
+                val quizList: List<QuizType> =
+                        gson.fromJson(requestBody,
+                                object : TypeToken<List<QuizType>>() {}.type)
+
+
+
             }
             else {
                 updateText = "Network Error"
             }
 
             uiThread {
-                newTextView.text = updateText
-                val intent = Intent(this, Login::class.java)
-                val newButton = Button(this)
-                linearLayoutResponse.addView(newTextView)
+                val intent = Intent(this@MainActivity, Login::class.java)
+                val newButton = Button(this@MainActivity)
+                newButton.setOnClickListener {
+                    intent.putExtra(USERNAME, username.text)
+                    intent.putExtra(PASSWORD, password.text)
+                    intent.putExtra(PASSWORD2, confirmation.text)
+                    startActivity(intent)
+                }
+
             }
         }
 
